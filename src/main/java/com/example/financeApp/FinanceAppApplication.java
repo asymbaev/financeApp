628b975427e858cbdb5e2ecfbf3f6ae1290c884e
Dastan <asymbaevdastan07@gmail.com>
2024-01-25 15:35:55 -0500

    Cmpltd

--- src/main/java/com/example/financeApp/FinanceAppApplication.java
+++ src/main/java/com/example/financeApp/FinanceAppApplication.java
@@ -1,13 +1,51 @@
 package com.example.financeApp;
 
+import com.example.financeApp.entity.Amount;
+import com.example.financeApp.entity.User;
+import com.example.financeApp.repository.AmountRepository;
+import com.example.financeApp.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FinanceAppApplication {
 
+import java.util.Arrays;
+import java.util.List;
+
+
+@SpringBootApplication
+public class FinanceAppApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private AmountRepository amountRepository;
+
+
+private static final Logger logger = LoggerFactory.getLogger(FinanceAppApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceAppApplication.class, args);
+		logger.info("Finance Application Started");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("Aestro", "12345", "user", "1", 5000);
+		User user2 = new User("Pcblt", "8736252", "user2", "2", 5000);
+		userRepository.save(user);
+		userRepository.save(user2);
+
+		List<Amount> amounts = Arrays.asList(
+				new Amount(2L,5000, "A long ago", 5000),
+				new Amount(3L, 5000, "a day ago", 5000)
+
+		);
+		amountRepository.saveAll(amounts);
+
+		userRepository.findAll().forEach(us -> logger.info(us.getUserId() + " " + us.getAmount()));
+		amountRepository.findAll().forEach(am -> logger.info(am.getHistory() + " " + am.getAmount()));
+
+	}
 }
--- src/main/java/com/example/financeApp/web/UserController.java
+++ src/main/java/com/example/financeApp/web/UserController.java
@@ -1,4 +1,26 @@
 package com.example.financeApp.web;
 
+
+import com.example.financeApp.entity.Amount;
+import com.example.financeApp.entity.User;
+import com.example.financeApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/matchByAmount")
+    public ResponseEntity<User> matchUserByAmount (@RequestBody Amount amount) {
+        User matchedUser = userService.matchUserByAmount(amount);
+        return ResponseEntity.ok(matchedUser);
+    }
 }
