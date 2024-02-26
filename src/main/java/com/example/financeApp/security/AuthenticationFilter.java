4b68ddef3197b2874a79f5c90ba71fb4ff45590b
Dastan <asymbaevdastan07@gmail.com>
2024-02-26 11:42:29 -0500

    Setting up a class Security config

--- src/main/java/com/example/financeApp/security/AuthenticationFilter.java
+++ src/main/java/com/example/financeApp/security/AuthenticationFilter.java
@@ -1,4 +1,23 @@
 package com.example.financeApp.security;
 
-public class AuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.net.http.HttpHeaders;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwsToken = request.getHeader(HttpHeaders)
+
+    }
 }
---
+++ src/main/java/com/example/financeApp/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.financeApp.security;
+
+public class SecurityConfig {
+}
