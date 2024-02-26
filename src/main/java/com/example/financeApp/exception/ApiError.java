2602923186e560a121e16b70a424c203607850ee
Dastan <asymbaevdastan07@gmail.com>
2024-02-26 11:52:41 -0500

    Setting up an exception folder

---
+++ src/main/java/com/example/financeApp/exception/ApiError.java
@@ -0,0 +1,4 @@
+package com.example.financeApp.exception;
+
+public class ApiError {
+}
--- src/main/java/com/example/financeApp/security/SecurityConfig.java
+++ src/main/java/com/example/financeApp/security/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.example.financeApp.security;
 
+import com.example.financeApp.exception.AuthEntryPoint;
+import com.example.financeApp.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private AuthenticationFilter authenticationFilter;
+    @Autowired
+    private AuthEntryPoint
 }
